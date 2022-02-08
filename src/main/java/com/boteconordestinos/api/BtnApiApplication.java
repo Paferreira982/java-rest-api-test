6a388b8b2b81cfdf5ddcc5f16aef51f4cf5d9f99
Paferreira982 <paferreira982@hotmail.com>
2022-02-08 11:08:51 -0300

    Insercao de arquivos

---
+++ src/main/java/com/boteconordestinos/api/BtnApiApplication.java
@@ -0,0 +1,13 @@
+package com.boteconordestinos.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtnApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtnApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boteconordestinos/api/BtnApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boteconordestinos.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtnApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
