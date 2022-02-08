f32b1b2e793ec5cde36b27cd64f165c5102e3747
Paferreira982 <paferreira982@hotmail.com>
2022-02-08 12:37:22 -0300

    Criacao do metodo de teste para a API

---
+++ src/main/java/com/boteconordestinos/api/repository/TesteRepository.java
@@ -0,0 +1,9 @@
+package com.boteconordestinos.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.boteconordestinos.api.model.Teste;
+
+public interface TesteRepository extends JpaRepository<Teste, Long>{
+
+}
--- src/main/java/com/boteconordestinos/api/resource/TestesResource.java
+++ src/main/java/com/boteconordestinos/api/resource/TestesResource.java
@@ -1,13 +1,24 @@
 package com.boteconordestinos.api.resource;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.boteconordestinos.api.model.Teste;
-
+import com.boteconordestinos.api.repository.TesteRepository;
 
 @RestController
 @RequestMapping("/teste")
 public class TestesResource {
-	Teste aux = new Teste();
+	
+	@Autowired
+	TesteRepository testeRepository;
+	
+	@GetMapping("/list")
+	public List<Teste> listAll() {
+		return testeRepository.findAll();
+	}
 }
